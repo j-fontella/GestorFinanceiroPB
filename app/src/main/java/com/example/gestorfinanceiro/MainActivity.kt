cb36be18882a72dfc2196608a13dedc1281b7749
Joao Lucs <joao.mbarreto@al.infnet.edu.br>
2020-03-11 22:50:35 -0300

    Adicionado fragmentos na tela de login

--- app/src/main/java/com/example/gestorfinanceiro/MainActivity.kt
+++ app/src/main/java/com/example/gestorfinanceiro/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.gestorfinanceiro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        botao_navegador.setupWithNavController(findNavController(R.id.fragment))
+
+
     }
 }
---
+++ app/src/main/java/com/example/gestorfinanceiro/fragments/RecuperarSenhaFragment.kt
@@ -0,0 +1,23 @@
+package com.example.gestorfinanceiro.fragments
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.gestorfinanceiro.R
+
+
+class RecuperarSenhaFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_recuperar_senha, container, false)
+    }
+}
